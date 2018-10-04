1776bf47d4de968b005db46b3c1570f64b8281ca
Hana (Hyang-Ah) Kim <hyangah@gmail.com>
2018-10-03 22:35:20 -0400

    update go.mod to reflect the real dependency and foobar

---
+++ cmd/foobar/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/hyangah/exp/lib1"
+
+func main() {
+  println("foobar", lib1.Name())
+}
--- go.mod
+++ go.mod
@@ -1,1 +1,3 @@
 module github.com/hyangah/exp
+
+require github.com/hyangah/exp/lib1 v1.0.0
